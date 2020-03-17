8b25a3ed3e005f25e3d36c68def35264b92ba309
Jatin Kumar Jain <31545613+Jatzj7@users.noreply.github.com>
2020-03-17 16:57:59 +0530

    added post details

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'post_list': posts
+    }
+    return render(request, "post_list.html", context)
+
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    context = {'post': post}
+    return render(request, "post_detail.html", context)
